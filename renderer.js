fcc1c5c3dde397e72071e918d7b668d84510c4a7
BenKung <moon791017@gmail.com>
2018-08-29 21:41:37 +0800

    fix url

--- renderer.js
+++ renderer.js
@@ -1,7 +1,7 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-//require('dotenv').config()//https://benwebknight.firebaseapp.com/
-let url='http://mdm.ose.com.tw/Account/Login?ReturnUrl=%2FHome%2FIndex'
+//require('dotenv').config()//
+let url='https://benwebknight.firebaseapp.com/'
 document.getElementById("view").setAttribute("src",url)
 
