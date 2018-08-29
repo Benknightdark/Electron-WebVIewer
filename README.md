317da4b0a75712d0aa78e07b60912b4b7d42bece
BenKung <moon791017@gmail.com>
2018-08-29 21:16:58 +0800

    add build command

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Usage
-* 修改.env檔案的url參數的網址
+* 修改render.js檔案的url參數的網址
 * 輸入npm start
--- renderer.js
+++ renderer.js
@@ -1,6 +1,7 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-require('dotenv').config()
-document.getElementById("view").setAttribute("src",process.env.url)
+//require('dotenv').config()//https://benwebknight.firebaseapp.com/
+let url='https://benwebknight.firebaseapp.com/'
+document.getElementById("view").setAttribute("src",url)
 
